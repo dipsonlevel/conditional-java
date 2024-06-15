3faa08a8cc996006cb6def0cc26b81f5b34a0ed3
dipson level <dipsonlevel@gmail.com>
2024-06-15 17:26:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let side1 = 3
+
+let side2 = 4
+
+let side3 = 5
+
+if ("side1" === "side2" && "side2" === "side3") {
+    console.log("Equilateral triangle")
+}else if("side1" === "side 2" || "side1" === "side3" || "side2" === "side 3"){
+    console.log("isosceles triangle")
+}else{
+    console.log("scalene triangle")
+}
